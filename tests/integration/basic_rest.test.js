fe61e3080cd651b372db9b82533ebdba91c56514
Stephen Walker <Stephen.d.a.walker@gmail.com>
2021-07-12 08:06:41 -0500

    Add integration test for different config preferences
    Add equivalent integration test configurations for endpoints in store config and in binding config.

--- tests/integration/basic_rest.test.js
+++ tests/integration/basic_rest.test.js
@@ -1,83 +1,87 @@
 import BlackBox from "./blackbox.js"
-import { plugin_config, store_config, resolve_data } from './basic_rest_config.js'
+import { scenarios, resolve_data } from './basic_rest_config.js'
 import Bind from "@src/exports.js"
 
-describe("basic scenario", () => {
+for ( let [ name, plugin_config, store_config ] of scenarios ) {
 
-  let bb = new BlackBox({
-    plugin_config, 
-    store_config,
-    resolve_data
-  });
-
-  bb.mock_axios();
-
-  beforeEach(() => {
-    bb.module.mockClear();
-  });
-
-  it("should start bindings when start bind is called", () => {
-    bb.input_state("profile", {
-      users: {}
+  describe(`basic scenario with ${name}`, () => {
+  
+    let bb = new BlackBox({
+      plugin_config, 
+      store_config,
+      resolve_data
     });
-    bb.dispatch("profile/start_bind");
-    return bb.output_state("profile", {
-      users : resolve_data["/users/"].data
+  
+    bb.mock_axios();
+  
+    beforeEach(() => {
+      bb.module.mockClear();
     });
-  });
-
-  it("should trigger login", () => {
-    bb.input_state("profile", {
-      username : "james",
-      password : "brond",
-      token    : ""
-    });
-    bb.dispatch("profile/trigger_login");
-    return bb.output_state("profile", {
-      username : "james",
-      password : "",
-      token    : "somesecuritytoken"
-    });
-  });
-
-  it("shouldn't trigger login when username is blank", () => {
-    bb.input_state("profile", {
-      username : "",
-      password : "brond",
-      token    : {}
-    });
-    bb.dispatch("profile/trigger_login");
-    return bb.output_state("profile", {
-      username : "",
-      password : "brond",
-      token    : {}
-    })
-  });
-
-  it("should trigger posts and then pull post_meta data", () => {
-    bb.input_state("profile", {
-      date : "2020-01-02" 
-    });
-    bb.dispatch("profile/trigger_posts");
-    return bb.output_state("profile", { 
-      date : "2020-01-02",
-      post_meta_data : resolve_data["/post/meta"].data,
-      post_ids : [1,2],
-      posts: resolve_data["/post/"].data,
-    });
-  });
-
-  describe("After start_bind has been called", () => {
-    bb.dispatch("profile/start_bind");
-
-    it("should call get data when selected_user_id is updated", () => {
+  
+    it("should start bindings when start bind is called", () => {
       bb.input_state("profile", {
-        user_data : {}
-      }), 
-      bb.commit("profile/update_selected_user_id", 10);
+        users: {}
+      });
+      bb.dispatch("profile/start_bind");
       return bb.output_state("profile", {
-        user_data : resolve_data["/user/data/"].data
+        users : resolve_data["/users/"].data
+      });
+    });
+  
+    it("should trigger login", () => {
+      bb.input_state("profile", {
+        username : "james",
+        password : "brond",
+        token    : ""
+      });
+      bb.dispatch("profile/trigger_login");
+      return bb.output_state("profile", {
+        username : "james",
+        password : "",
+        token    : "somesecuritytoken"
+      });
+    });
+  
+    it("shouldn't trigger login when username is blank", () => {
+      bb.input_state("profile", {
+        username : "",
+        password : "brond",
+        token    : {}
+      });
+      bb.dispatch("profile/trigger_login");
+      return bb.output_state("profile", {
+        username : "",
+        password : "brond",
+        token    : {}
+      })
+    });
+  
+    it("should trigger posts and then pull post_meta data", () => {
+      bb.input_state("profile", {
+        date : "2020-01-02" 
+      });
+      bb.dispatch("profile/trigger_posts");
+      return bb.output_state("profile", { 
+        date : "2020-01-02",
+        post_meta_data : resolve_data["/post/meta"].data,
+        post_ids : [1,2],
+        posts: resolve_data["/post/"].data,
+      });
+    });
+  
+    describe("After start_bind has been called", () => {
+      bb.dispatch("profile/start_bind");
+  
+      it("should call get data when selected_user_id is updated", () => {
+        bb.input_state("profile", {
+          user_data : {}
+        }), 
+        bb.commit("profile/update_selected_user_id", 10);
+        return bb.output_state("profile", {
+          user_data : resolve_data["/user/data/"].data
+        });
       });
     });
   });
-});
+
+}
--- tests/integration/basic_rest_config.js
+++ tests/integration/basic_rest_config.js
@@ -1,41 +1,285 @@
-export const plugin_config = {
-  strict        : true,
-  sources : {
-    url : "http://myapi.hppr.dev/api",
-  },
-  endpoints : {
-    users : {
-      type : Array
+
+export const scenarios = [
+[
+  "endpoints in plugin config",
+  { // Plugin Config
+    strict        : true,
+    sources : {
+      url : "http://myapi.hppr.dev/api",
     },
-    user_data : {
-      url       : "/user/data/",
-      params    : {
-        user_id : Number
-      }
-    },
-    login : {
-      url       : "/login",
-      method    : "post",
-      params    : {
-        username : String,
-        password : String,
+    endpoints : {
+      users : {
+        type : Array
       },
-    },
-    posts : {
-      url    : "/post/",
-      type   : Array,
-      params : {
-        date : String
+      user_data : {
+        url       : "/user/data/",
+        params    : {
+          user_id : Number
+        }
+      },
+      login : {
+        url       : "/login",
+        method    : "post",
+        params    : {
+          username : String,
+          password : String,
+        },
+      },
+      posts : {
+        url    : "/post/",
+        type   : Array,
+        params : {
+          date : String
+        }
+      },
+      post_meta : {
+        url : "/post/meta",
+        params : {
+          ids : Array
+        }
       }
+    }
+  },
+  { // Store Config
+    profile : {
+      namespace : "profile",
+      state     : {
+        selected_user_id : 0,
+        posts    : [],
+        post_ids : [],
+        post_meta_data : {},
+        token    : [],
+      },
+      mutations : {
+        update_selected_user_id : ( state, value ) => state.selected_user_id = value,
+        update_posts_and_meta_ids : ( state, posts ) => {
+          state.posts = posts;
+          state.post_ids = posts.map((p) => p.id);
+        },
+        update_token : (state, data) => {
+          state.token = data.token;
+          state.password = "";
+        },
+      },
+      bindings : {
+        login : {
+          bind_type : "trigger",
+          redirect  : "update_token",
+          create_params : true,
+        },
+        user_data : {
+          bind_type : "watch",
+          period    : 10000,
+          param_map : {
+            selected_user_id : "user_id",
+          },
+        },
+        users : {
+          bind_type : "once"
+        },
+        posts : {
+          bind_type     : "trigger",
+          create_params : true,
+          redirect      : "update_posts_and_meta_ids",
+          side_effect   : "trigger_post_meta_data",
+        },
+        post_meta_data : {
+          endpoint  : "post_meta",
+          bind_type : "trigger",
+          param_map : {
+            post_ids : "ids"
+          },
+        }
+      }
+    }
+  },
+],[
+  "endpoints in store config",
+  { // Plugin Config
+    strict        : true,
+    sources : {
+      url : "http://myapi.hppr.dev/api",
     },
-    post_meta : {
-      url : "/post/meta",
-      params : {
-        ids : Array
+  },
+  { // Store Config
+    profile : {
+      namespace : "profile",
+      state     : {
+        selected_user_id : 0,
+        posts    : [],
+        post_ids : [],
+        post_meta_data : {},
+        token    : [],
+      },
+      endpoints : {
+        users : {
+          type : Array
+        },
+        user_data : {
+          url       : "/user/data/",
+          params    : {
+            user_id : Number
+          }
+        },
+        login : {
+          url       : "/login",
+          method    : "post",
+          params    : {
+            username : String,
+            password : String,
+          },
+        },
+        posts : {
+          url    : "/post/",
+          type   : Array,
+          params : {
+            date : String
+          }
+        },
+        post_meta : {
+          url : "/post/meta",
+          params : {
+            ids : Array
+          }
+        }
+      },
+      mutations : {
+        update_selected_user_id : ( state, value ) => state.selected_user_id = value,
+        update_posts_and_meta_ids : ( state, posts ) => {
+          state.posts = posts;
+          state.post_ids = posts.map((p) => p.id);
+        },
+        update_token : (state, data) => {
+          state.token = data.token;
+          state.password = "";
+        },
+      },
+      bindings : {
+        login : {
+          bind_type : "trigger",
+          redirect  : "update_token",
+          create_params : true,
+        },
+        user_data : {
+          bind_type : "watch",
+          period    : 10000,
+          param_map : {
+            selected_user_id : "user_id",
+          },
+        },
+        users : {
+          bind_type : "once"
+        },
+        posts : {
+          bind_type     : "trigger",
+          create_params : true,
+          redirect      : "update_posts_and_meta_ids",
+          side_effect   : "trigger_post_meta_data",
+        },
+        post_meta_data : {
+          endpoint  : "post_meta",
+          bind_type : "trigger",
+          param_map : {
+            post_ids : "ids"
+          },
+        }
+      }
+    }
+  },
+],[
+  "endpoints in bindings config",
+  { // Plugin Config
+    strict        : true,
+    sources : {
+      url : "http://myapi.hppr.dev/api",
+    },
+  },
+  { // Store Config
+    profile : {
+      namespace : "profile",
+      state     : {
+        selected_user_id : 0,
+        posts    : [],
+        post_ids : [],
+        post_meta_data : {},
+        token    : [],
+      },
+      mutations : {
+        update_selected_user_id : ( state, value ) => state.selected_user_id = value,
+        update_posts_and_meta_ids : ( state, posts ) => {
+          state.posts = posts;
+          state.post_ids = posts.map((p) => p.id);
+        },
+        update_token : (state, data) => {
+          state.token = data.token;
+          state.password = "";
+        },
+      },
+      bindings : {
+        login : {
+          bind_type : "trigger",
+          redirect  : "update_token",
+          create_params : true,
+          endpoint : {
+            url       : "/login",
+            method    : "post",
+            params    : {
+              username : String,
+              password : String,
+            },
+          },
+        },
+        user_data : {
+          bind_type : "watch",
+          period    : 10000,
+          param_map : {
+            selected_user_id : "user_id",
+          },
+          endpoint : {
+            url       : "/user/data/",
+            params    : {
+              user_id : Number
+            }
+          },
+        },
+        users : {
+          endpoint : {
+            type : Array
+          },
+          bind_type : "once"
+        },
+        posts : {
+          bind_type     : "trigger",
+          create_params : true,
+          redirect      : "update_posts_and_meta_ids",
+          side_effect   : "trigger_post_meta_data",
+          endpoint : {
+            url    : "/post/",
+            type   : Array,
+            params : {
+              date : String
+            }
+          },
+        },
+        post_meta_data : {
+          endpoint  : "post_meta",
+          bind_type : "trigger",
+          param_map : {
+            post_ids : "ids"
+          },
+          endpoint : {
+            url : "/post/meta",
+            params : {
+              ids : Array
+            }
+          }
+        }
       }
     }
   }
-};
+],
+]
+
 
 export const resolve_data = {
     '/users/'     : { data : ["jenkins", "flores", "jamie"] },
@@ -43,58 +287,4 @@ export const resolve_data = {
     '/login'      : { data : { token : "somesecuritytoken" }},
     '/post/'      : { data : [ { id: 1, text : "let me tell you about"}, { id: 2, text : "dinosaurs are the best" } ] },
     '/post/meta'  : { data : { date_created : "100BC", likes : 0, shares : 1 }},
-};
-
-export const store_config = {
-  profile_store : {
-    namespace : "profile",
-    state     : {
-      selected_user_id : 0,
-      posts    : [],
-      post_ids : [],
-      post_meta_data : {},
-      token    : [],
-    },
-    mutations : {
-      update_selected_user_id : ( state, value ) => state.selected_user_id = value,
-      update_posts_and_meta_ids : ( state, posts ) => {
-        state.posts = posts;
-        state.post_ids = posts.map((p) => p.id);
-      },
-      update_token : (state, data) => {
-        state.token = data.token;
-        state.password = "";
-      },
-    },
-    bindings : {
-      login : {
-        bind_type : "trigger",
-        redirect  : "update_token",
-        create_params : true,
-      },
-      user_data : {
-        bind_type : "watch",
-        period    : 10000,
-        param_map : {
-          selected_user_id : "user_id",
-        },
-      },
-      users : {
-        bind_type : "once"
-      },
-      posts : {
-        bind_type     : "trigger",
-        create_params : true,
-        redirect      : "update_posts_and_meta_ids",
-        side_effect   : "trigger_post_meta_data",
-      },
-      post_meta_data : {
-        endpoint  : "post_meta",
-        bind_type : "trigger",
-        param_map : {
-          post_ids : "ids"
-        },
-      }
-    }
-  }
 };
